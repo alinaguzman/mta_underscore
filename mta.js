47c0def6817c0204dea59f98872a948ed1961225
Alina Guzman <alinochka8@gmail.com>
2013-07-25 16:45:31 -0400

    Added prompts and function to display stops for cross-line trips. Its hard coded with Union Sq as the intersection for now

--- mta.js
+++ mta.js
@@ -1,89 +1,88 @@
 // Refactor your code with underscore
-// Prompt the user
-// Please choose a startLine
-// Please chooose a startStation
-// Please choose an endLine
-// Please choose an endStation
+// xx Prompt the user
+// xx Please choose a startLine
+// xx Please choose a startStation
+// xx Please choose an endLine
+// xx Please choose an endStation
 // Find the intersection
 // Display the number of stops
 // Track the total journeys taken
 // Calculate total cost of journeys taken at $2.50 per ride.
 
-function Train(name, stations) {
-  this.name = name;
-  this.stations = stations;
-}
-
-Train.prototype.distance = function(board, exit) {
-  board = this.stations.indexOf(board);
-  exit = this.stations.indexOf(exit);
-  return Math.abs(board - exit);
-};
-
 var lStations = [ "8th", "6th", "Union Square", "3rd", "1st" ];
 var nStations = [ "Times Square", "34th", "28th", "23rd", "Union Square", "8th" ];
 var sixStations = [ "Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place" ];
 var gStations = [ "Greenpoint", "Nassau", "Metropolitan", "Broadway" ];
-var sStations = [ "Greenpoint", "Nassau", "Metropolitan", "Broadway" ];
+
+function Train(name, stations) {
+    this.name = name;
+    this.stations = stations;
+}
 
 var lTrain = new Train('The L Train', lStations);
 var nTrain = new Train('The N Train', nStations);
 var sixTrain = new Train('The Six Train', sixStations);
 var gTrain = new Train('The G Train', gStations);
 
-var trains = [lTrain, nTrain, sixTrain, gTrain, sTrain];
+Train.prototype.distance = function(board, exit) {
+    board = this.stations.indexOf(board);
+    exit = this.stations.indexOf(exit);
+    return Math.abs(board - exit);
+};
 
-var msg = "Which train would you \nlike to get on?" + displayLines();
-var startTrain = prompt(msg);
+trains = [lTrain, nTrain, sixTrain, gTrain];
 
-var trains = [lTrain, nTrain, sixTrain, gTrain];
-
-var msg = "Which train would you \nlike to get on?\n" + displayLines();
-var startTrain = prompt(msg);
-
-function displayLines() {
-  var trainNames = "";
-  for (var i = 0; i < trains.length; i++) {
-    trainNames += trains[i].name + "\n";
-  }
-
-  forEach(trains, trainName);
-
-  return trainNames.trim();
-}
-
-
-function trainName(train){
-  return train.name + "\n"
-}
-
-function forEach(array, func) {
-  for (var i = 0; i < array.length; i++) {
-    func(array[i]);
-  }
-}
-
-function map(array, func) {
-  var newArray = [];
-  for (var i = 0; i < array.length; i++) {
-    newArray.push(func(array[i]));
-  }
-  return newArray;
-}
-
-function displayStations() {
-  var train = null;
-  for (var j = 0; j < trains.length; j++) {
-    if (trains[j].name === startTrain) {
-      train = trains[j];
+//Displays Lines on the first user prompt
+function displayLines(){
+    var trainNames = "";
+    for (var i = 0; i< trains.length; i++) {
+        trainNames += trains[i].name + "\n";
     }
-  }
-  var trainStations = "";
-  for (var k = 0; k < train.stations.length; k++) {
-    trainStations += train.stations[k] + "\n";
-  }
-  return trainStations.trim();
+    return trainNames.trim();
 }
 
-var msg2 = "Which station would you \nlike to get on?\n" + displayStations();
-var startStation = prompt(msg2);
+//sets users choice as an index number
+var trainMsg = "Which train would you like to get on?\n" + displayLines();
+firstTrain = parseFloat(prompt(trainMsg));
+
+//function to display stops for the first line
+function displayStops() {
+    var trainStops = "";
+    for (var i = 0; i < trains[firstTrain].stations.length; i++ ){
+        trainStops += trains[firstTrain].stations[i] + "\n";
+    }
+    return trainStops;
+}
+
+//prompt to choose first trains stop
+var firstStopMsg = "Which stop would you like to get on?\n";
+firstStop = prompt(firstStopMsg + displayStops());    //this returns a station name ex "8th"
+
+//prompt to choose second train line
+var trainMsg2 = "Which train would you like to get off?\n" + displayLines();
+lastTrain = parseFloat(prompt(trainMsg2));
+
+//displays second prompt with list of stations of the second line they choose
+function displaySecondStops() {
+    var trainStops = "";
+    for (var i = 0; i < trains[lastTrain].stations.length; i++) {
+        trainStops += trains[lastTrain].stations[i] + "\n";
+    }
+    return trainStops;
+}
+
+//prompts user to choose their second lines station to get off.
+var secondStopMsg = "Which stop would you like to get off?\n";
+secondStop = prompt(secondStopMsg + displaySecondStops()); //returns a string station name
+
+//Doesn't work for G train because I hard coded in Union Square
+if (firstTrain === lastTrain){
+    alert(trains[firstTrain].distance(firstStop,secondStop) + " stops.");
+} else {
+    var first = Math.abs(trains[firstTrain].distance(firstStop, "Union Square"));
+    var second = Math.abs(trains[lastTrain].distance(secondStop, "Union Square"));
+    var totalDistance = Math.abs(first + second);
+
+    totalDistance = Math.abs(first + second)
+    alert(totalDistance + " stops.")
+}
